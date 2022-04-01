f737b988018d491e58e88706840eacff21ac0444
Jay <depuytjerome@gmail.com>
2022-04-01 16:03:49 +0200

    swagger

--- Exercices/ex-API/app.js
+++ Exercices/ex-API/app.js
@@ -2,6 +2,25 @@ const express = require('express');
 const fruitsRouter = require('./routes/fruits-route');
 const app = express();
 
+// SWAGGER
+const swaggerJsDoc = require('swagger-jsdoc');
+const options = {
+    swaggerDefinition: {
+        info: {
+            title: 'API exemple',
+            title: 'Recap sur les API',
+            version: '0.1.0',
+        }
+    },
+    apis: [
+        './routes/fruits-route.js'
+    ]
+}
+
+const swaggerSpec = swaggerJsDoc(options);
+const swaggerUI = require('swagger-ui-express')
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+
 // pour nos requètes .post
 app.use(express.json());
 app.use(fruitsRouter);
--- Exercices/ex-API/routes/fruits-route.js
+++ Exercices/ex-API/routes/fruits-route.js
@@ -1,12 +1,62 @@
 const fruitsController = require('../controllers/fruits-controller');
 const fruitsRouter = require('express').Router();
 
-fruitsRouter.get('/fruit', fruitsController.getOne);
-fruitsRouter.get('/fruit', fruitsController.getAll);
+/**
+ * @swagger
+ * /fruit :
+ */
 
+/**
+ * @openapi
+ * /fruit :
+ *  get: 
+ *      description: Permet de récupérer tout les fruits
+ *      responses:
+ *          200:
+ *              description: Renvoi la liste de fruits
+ */
+fruitsRouter.get('/fruit', fruitsController.getAll);
+/**
+ * @openapi
+ * /fruit :
+ *  post:
+ *      description: Permet d'ajouter un fruit
+ *      responses:
+ *          200:
+ *              description: Renvoi le fruit ajouté
+ */
 fruitsRouter.post('/fruit', fruitsController.insert);
-fruitsRouter.put('/fruit', fruitsController.update);
-fruitsRouter.delete('/fruit', fruitsController.delete);
+
+/**
+ * @openapi
+ * /fruit/{id} :
+ *  get: 
+ *      description: Permet de récupérer un fruit
+ *      responses:
+ *          200:
+ *              description: Renvoi le fruit
+ */
+fruitsRouter.get('/fruit/:id', fruitsController.getOne);
+/**
+ * @openapi
+ * /fruit/{id} :
+ *  put: 
+ *      description: Permet de mettre à jour un fruit
+ *      responses:
+ *          501:
+ *              description: Pas encore implémenté
+ */
+fruitsRouter.put('/fruit/:id', fruitsController.update);
+/**
+ * @openapi
+ * /fruit/{id} :
+ *  delete: 
+ *      description: Permet de supprimer un fruit
+ *      responses:
+ *          200:
+ *              description: Renvoi ...
+ */
+fruitsRouter.delete('/fruit/:id', fruitsController.delete);
 
 // to : app.js
 module.exports = fruitsRouter;
